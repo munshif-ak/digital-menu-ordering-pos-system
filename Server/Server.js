440c045daa02dfd8e2792d97b47eabbf4193dc42
Mohammed Munshif <akmunshif@gmail.com>
2025-08-24 01:21:02 +0530

    initial Express/MongoDB setup

---
+++ Server/Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT;
+// const categoryRoutes = require("./routes/categoryRoute");
+
+app.use((req, res, next) => {
+  console.log("path " + req.path + "method " + req.method);
+  next();
+});
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`DB connected ,app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// app.use("/api/menus/category", categoryRoutes);
+
